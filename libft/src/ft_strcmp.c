85cfe385b9b0f6c992d9156ffedc4fce27ea7fd9
Evgenii Balabanov <e.balabanov@apollo4u.net>
2021-01-03 11:49:19 +0300

    fix bag in strcmp

--- libft/src/ft_strcmp.c
+++ libft/src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: telron <telron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 08:05:34 by telron            #+#    #+#             */
-/*   Updated: 2020/12/07 08:05:59 by telron           ###   ########.fr       */
+/*   Updated: 2021/01/03 11:48:44 by telron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ static size_t	ft_min(size_t first, size_t second)
 
 int				ft_strcmp(const char *str1, const char *str2)
 {
-	return (ft_memcmp(str1, str2, ft_min(ft_strlen(str1), ft_strlen(str2))));
+	return (ft_memcmp(str1, str2,\
+		ft_min(ft_strlen(str1), ft_strlen(str2)) + 1));
 }
